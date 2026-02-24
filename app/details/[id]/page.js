46a23b0c4a57036c6e285f0418cafdf8e1f5d166
Somel Ahmed <progsomel@Somels-MacBook-Pro.local>
2026-02-24 12:35:14 +0600

    Introduction & Project Structure

---
+++ app/details/[id]/page.js
@@ -0,0 +1,22 @@
+import EventDetails from "@/components/details/EventDetails";
+import EventVenue from "@/components/details/EventVenue";
+import HeroSection from "@/components/details/HeroSection";
+import React from "react";
+
+export default function DetailsPage() {
+  return (
+    <>
+      <HeroSection />
+      <section className="container">
+        <div
+          className="grid grid-cols-5 gap-12
+        my-12
+        "
+        >
+          <EventDetails />
+          <EventVenue />
+        </div>
+      </section>
+    </>
+  );
+}
