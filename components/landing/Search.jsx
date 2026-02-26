0eb81db87a03b49072f3ea14faa05453f3bc0ae2
Somel Ahmed <progsomel@Somels-MacBook-Pro.local>
2026-02-26 10:22:23 +0600

    updated readme.md

---
+++ components/landing/Search.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Search() {
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search..."
+        className="bg-[#27292F] border border-[#CCCCCC]/20 py-1 px-2 rounded-md"
+      />
+    </div>
+  );
+}
